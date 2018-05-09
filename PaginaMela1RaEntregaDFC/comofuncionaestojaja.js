c5e3ce94a796f4c784ca8c01a6187dcac4ecc9d1
Daniel Fernando Castañeda <36462072+dfcastanedat@users.noreply.github.com>
2018-05-08 22:51:25 -0400

    Add files via upload

---
+++ PaginaMela1RaEntregaDFC/comofuncionaestojaja.js
@@ -0,0 +1,5 @@
+<script>
+// Este un comentario de una única línea
+alert("No se jajajajajaja xdxd"); //Aquí puedo poner una nota de lo que hace esta línea
+// alert("Esto no se ejecuta");
+</script>
