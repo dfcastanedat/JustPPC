7f1936a22e8a1fd05c65557c6fd541aa94520d92
Daniel Fernando Castañeda <36462072+dfcastanedat@users.noreply.github.com>
2018-05-13 22:07:22 -0400

    Add files via upload
    2nd Iteration

---
+++ PaginaMelaV2/comofuncionaestojaja.js
@@ -0,0 +1,4 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function sigo_sin_saber() {
+    print("Mk que no sé jajajaaj")
+}
