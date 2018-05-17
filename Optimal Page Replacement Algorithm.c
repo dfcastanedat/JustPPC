0f2570fed9b317e8940cb767e2c934027b2510f5
Luis Alejandro Higuaran Serrano <36466374+LAHIGUARANS@users.noreply.github.com>
2018-05-16 21:51:45 -0500

    Update Optimal Page Replacement Algorithm.c

--- Optimal Page Replacement Algorithm.c
+++ Optimal Page Replacement Algorithm.c
@@ -77,14 +77,14 @@ void optimalPage(int pg[], int pn, int fn)
     cout << "No. of hits = " << hit << endl;
     cout << "No. of misses = " << pn - hit << endl;
 }
-void generateReferenceString(int size_pg)
+void generateReferenceString(int size_pg, int random_limit)
 {
    int i;
    srand(time(0));
    ReferenceString = (int *)malloc( sizeof(int) * size_pg );
    for(i=0; i< size_pg; i++)
    {
-	ReferenceString[i] = rand() % ReferenceSZ;
+	ReferenceString[i] = rand() % random_limit;
    }
    return ReferenceString;
 }
@@ -94,6 +94,7 @@ int main()
 {
     //Number of frames
     int number_frames,size_pg;
+    int random_limit = 8;
     int pg[];
     printf("Enter number of frames: ");
     scanf("%d", &number_frames);}
@@ -102,7 +103,7 @@ int main()
     scanf("%d", &size_pg);
     printf("\n");
     
-    int pg[] = generateReferenceString(size_pg);
+    int pg[] = generateReferenceString(size_pg,random_limit);
     printf("The randomized Reference String: ");
     for(i=0; i< size_pg; i++)
    {
